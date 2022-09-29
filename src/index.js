1906e586ba35b2688c1f25009d5f67befd599c0a
updatedMyJournal <99217422+updatedMyJournal@users.noreply.github.com>
2022-09-29 22:51:10 +0300

    Implement tab switching

--- src/index.js
+++ src/index.js
@@ -7,3 +7,43 @@ import { insertContactContent } from './modules/contact.js';
 
 appendInitialDOMElements();
 insertHomeContent();
+
+const buttonsContainer = document.querySelector('nav');
+const homeButton = document.querySelector('nav .home');
+
+selectButton(homeButton);
+
+buttonsContainer.onclick = (e) => {
+  const clickedButton = e.target.closest('button');
+
+  if (!clickedButton || clickedButton.classList.contains('selected')) return;
+
+  selectButton(clickedButton);
+
+  const buttonClass = Object.values(clickedButton.classList).at(0);
+
+  switch(buttonClass) {
+    case 'home':
+      insertHomeContent();
+      break;
+    case 'menu':
+      insertMenuContent();
+      break;
+    case 'contact':
+      insertContactContent();
+      break;
+  }
+
+  scrollTo(0, 0);
+};
+
+function selectButton(button) {
+  deSelectOtherButtons();
+  button.classList.add('selected');
+}
+
+function deSelectOtherButtons() {
+  buttonsContainer
+    .querySelectorAll('button')
+    .forEach((button) => button.classList.remove('selected'));
+}
